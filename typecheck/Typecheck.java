b1b4eefbb3b424708457c5a5d4ae32164c525ced
Annie Chiou <anniechiou4@gmail.com>
2019-01-24 13:49:55 -0800

    feat: add VisitorPhaseOne class

--- typecheck/Typecheck.java
+++ typecheck/Typecheck.java
@@ -1,7 +1,17 @@
-import syntaxtree;
+package typecheck;
+
+import syntaxtree.*;
+import typecheck.*;
 
 public class Typecheck {
     public static void main(String[] args) {
-        new MiniJavaParser(System.in);
+        try {
+            new MiniJavaParser(System.in);
+            Goal program = MiniJavaParser.Goal();
+            VisitorPhaseOne visitor = new VisitorPhaseOne();
+            program.accept(visitor);
+        } catch (ParseException e) {
+            System.out.println("ParseException in Typecheck main");
+        }
     }
 }

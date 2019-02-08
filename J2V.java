3f1fc04487ffc39f15b205babc4c80a8ceca0ca3
Annie Chiou <anniechiou4@gmail.com>
2019-02-07 16:01:03 -0800

    fix: clean up FirstVisitor

--- J2V.java
+++ J2V.java
@@ -1,13 +1,9 @@
 import syntaxtree.*;
 
 public class J2V {
-    public static void main(String[] args) {
-        try {
-            new MiniJavaParser(System.in);
-            Goal program = MiniJavaParser.Goal();
-
-        } catch (ParseException e) {
-            return;
-        }
+    public static void main(String[] args) throws ParseException {
+        new MiniJavaParser(System.in);
+        Goal program = MiniJavaParser.Goal();
+        program.accept(new FirstVisitor()); // Pass in visitor
     }
 }

2cd9f5674a7c001391f55e34418d65d81cf7d79d
Dakota <dakotabwhelchel@gmail.com>
2019-02-07 17:51:20 -0800

    added some stuff

--- ClassRecord.java
+++ ClassRecord.java
@@ -2,4 +2,20 @@ import java.util.Map;
 
 public class ClassRecord {
 
+    // map of class names to methods
+    private Map<String, Map<String, String>> vtable = new LinkedHashMap<>();
+
+    // map of class names to fields
+    private Map<String, ArrayList<String>> record = new LinkedHashmap<>();
+
+    // put class name and method maps (maps field names to offsets)
+    public putFunction(String str, Map<String, String> map) {
+
+    }
+
+    // put class name and field maps (maps field names to offsets)
+    public putRecords(String str, Map<String, String> map) {
+
+    }
+
 }

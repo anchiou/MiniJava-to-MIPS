00f7979fb073c29027dc931f6bfb5c63c378d4f6
Annie Chiou <anniechiou4@gmail.com>
2019-02-14 16:04:13 -0800

    feat: add ClassManager methods

--- ClassManager.java
+++ ClassManager.java
@@ -6,16 +6,15 @@ public class ClassManager {
     // map of class names to fields
     private Map<String, ArrayList<String>> record = new LinkedHashMap<>();
     // map of class names to methods
-    private Map<String, Map<String, String>> vtable = new LinkedHashMap<>();
+    private Map<String, Map<String, String>> vTable = new LinkedHashMap<>();
 
-    // put class name and field maps (maps field names to offsets)
-    public void putRecords(String str, Map<String, String> map) {
-
+    // put class name and fields list
+    public void putRecords(String class, ArrayList<String> fields) {
+        this.record.put(class, fields);
     }
 
     // put class name and method maps (maps field names to offsets)
-    public void putFunction(String str, Map<String, String> map) {
-
+    public void putMethods(String class, Map<String, String> methods) {
+        this.vTable.put(class, methods);
     }
-
 }

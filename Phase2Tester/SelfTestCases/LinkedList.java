d85d6cee063d0b82ee8f69a3373d2bff8abecf08
Annie Chiou <anniechiou4@gmail.com>
2019-02-16 22:17:02 -0800

    fix: indentation in test files

--- Phase2Tester/SelfTestCases/LinkedList.java
+++ Phase2Tester/SelfTestCases/LinkedList.java
@@ -5,7 +5,7 @@ class LinkedList{
 }
 
 class Element {
-    int Age ;          
+    int Age ;
     int Salary ;
     boolean Married ;
 
@@ -20,7 +20,7 @@ class Element {
     public int GetAge(){
 	return Age ;
     }
-    
+
     public int GetSalary(){
 	return Salary ;
     }
@@ -30,7 +30,7 @@ class Element {
     }
 
     // This method returns true if the object "other"
-    // has the same values for age, salary and 
+    // has the same values for age, salary and
     public boolean Equal(Element other){
 	boolean ret_val ;
 	int aux01 ;
@@ -40,11 +40,11 @@ class Element {
 
 	aux01 = other.GetAge();
 	if (!this.Compare(aux01,Age)) ret_val = false ;
-	else { 
+	else {
 	    aux02 = other.GetSalary();
 	    if (!this.Compare(aux02,Salary)) ret_val = false ;
-	    else 
-		if (Married) 
+	    else
+		if (Married)
 		    if (!other.GetMarried()) ret_val = false;
 		    else nt = 0 ;
 		else
@@ -89,7 +89,7 @@ class List{
 	next = v_next ;
 	return true ;
     }
-    
+
     // Insert a new node at the beginning of the list
     public List Insert(Element new_elem){
 	boolean ret_val ;
@@ -100,14 +100,14 @@ class List{
 	ret_val = aux02.InitNew(new_elem,aux03,false);
 	return aux02 ;
     }
-    
-    
+
+
     // Update the the pointer to the next node
     public boolean SetNext(List v_next){
 	next = v_next ;
 	return true ;
     }
-    
+
     // Delete an element e from the list
     public List Delete(Element e){
 	List my_head ;
@@ -131,15 +131,15 @@ class List{
 	while ((!var_end) && (!ret_val)){
 	    if (e.Equal(var_elem)){
 		ret_val = true ;
-		if (aux04 < 0) { 
+		if (aux04 < 0) {
 		    // delete first element
 		    my_head = aux01.GetNext() ;
-		} 
+		}
 		else{ // delete a non first element
 		    System.out.println(0-555);
 		    aux05 = prev.SetNext(aux01.GetNext());
 		    System.out.println(0-555);
-		    
+
 		}
 	    } else nt = 0 ;
 	    if (!ret_val){
@@ -147,13 +147,13 @@ class List{
 		aux01 = aux01.GetNext() ;
 		var_end = aux01.GetEnd();
 		var_elem = aux01.GetElem();
-		aux04 = 1 ; 
+		aux04 = 1 ;
 	    } else nt = 0 ;
 	}
 	return my_head ;
     }
-    
-    
+
+
     // Search for an element e on the list
     public int Search(Element e){
 	int int_ret_val ;
@@ -177,20 +177,20 @@ class List{
 	}
 	return int_ret_val ;
     }
-    
+
     public boolean GetEnd(){
 	return end ;
     }
-    
+
     public Element GetElem(){
 	return elem ;
     }
-    
+
     public List GetNext(){
 	return next ;
     }
-    
-    
+
+
     // Print the linked list
     public boolean Print(){
 	List aux01 ;
@@ -210,69 +210,65 @@ class List{
 	return true ;
     }
 }
-    
+
 
 // this class invokes the methods to insert, delete,
 // search and print the linked list
 class LL{
 
     public int Start(){
+		List head ;
+		List last_elem ;
+		boolean aux01 ;
+		Element el01 ;
+		Element el02 ;
+		Element el03 ;
 
-	List head ;
-	List last_elem ;
-	boolean aux01 ;
-	Element el01 ;
-	Element el02 ;
-	Element el03 ;
+		last_elem = new List();
+		aux01 = last_elem.Init();
+		head = last_elem ;
+		aux01 = head.Init();
+		aux01 = head.Print();
 
-	last_elem = new List();
-	aux01 = last_elem.Init();
-	head = last_elem ;
-	aux01 = head.Init();
-	aux01 = head.Print();
+		// inserting first element
+		el01 = new Element();
+		aux01 = el01.Init(25,37000,false);
+		head = head.Insert(el01);
+		aux01 = head.Print();
+		System.out.println(10000000);
+		// inserting second  element
+		el01 = new Element();
+		aux01 = el01.Init(39,42000,true);
+		el02 = el01 ;
+		head = head.Insert(el01);
+		aux01 = head.Print();
+		System.out.println(10000000);
+		// inserting third element
+		el01 = new Element();
+		aux01 = el01.Init(22,34000,false);
+		head = head.Insert(el01);
+		aux01 = head.Print();
+		el03 = new Element();
+		aux01 = el03.Init(27,34000,false);
+		System.out.println(head.Search(el02));
+		System.out.println(head.Search(el03));
+		System.out.println(10000000);
+		// inserting fourth element
+		el01 = new Element();
+		aux01 = el01.Init(28,35000,false);
+		head = head.Insert(el01);
+		aux01 = head.Print();
+		System.out.println(2220000);
 
-	// inserting first element
-	el01 = new Element();
-	aux01 = el01.Init(25,37000,false);
-	head = head.Insert(el01);
-	aux01 = head.Print();
-	System.out.println(10000000);
-	// inserting second  element
-	el01 = new Element();
-	aux01 = el01.Init(39,42000,true);
-	el02 = el01 ;
-	head = head.Insert(el01);
-	aux01 = head.Print();
-	System.out.println(10000000);
-	// inserting third element
-	el01 = new Element();
-	aux01 = el01.Init(22,34000,false);
-	head = head.Insert(el01);
-	aux01 = head.Print();
-	el03 = new Element();
-	aux01 = el03.Init(27,34000,false);
-	System.out.println(head.Search(el02));
-	System.out.println(head.Search(el03));
-	System.out.println(10000000);
-	// inserting fourth element
-	el01 = new Element();
-	aux01 = el01.Init(28,35000,false);
-	head = head.Insert(el01);
-	aux01 = head.Print();
-	System.out.println(2220000);
-
-	head = head.Delete(el02);
-	aux01 = head.Print();
-	System.out.println(33300000);
+		head = head.Delete(el02);
+		aux01 = head.Print();
+		System.out.println(33300000);
 
 
-	head = head.Delete(el01);
-	aux01 = head.Print();
-	System.out.println(44440000);
-	
-	return 0 ;
-	
-	
+		head = head.Delete(el01);
+		aux01 = head.Print();
+		System.out.println(44440000);
+
+		return 0 ;
     }
-    
 }

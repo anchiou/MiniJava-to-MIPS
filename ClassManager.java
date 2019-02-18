3c3456040fada9abe66fbec7d438784569a50cb5
Annie Chiou <anniechiou4@gmail.com>
2019-02-17 22:40:55 -0800

    feat: offset functions in class manager

--- ClassManager.java
+++ ClassManager.java
@@ -8,24 +8,49 @@ public class ClassManager {
     // map of class names to methods: inner map is for overridden methods
     private Map<String, Map<String, String>> vTable = new LinkedHashMap<>(); // e.g., <class, <overriden method, class>>
 
-    // returns all class records
+    // return all class records
     public Map<String, ArrayList<String>> getAllRecords() {
         return this.record;
     }
 
-    // returns all class v-tables
+    // return all class v-tables
     public Map<String, Map<String, String>> getAllVTables() {
         return this.vTable;
     }
 
-    // returns corresponding class record
+    // return size of class record
+    public int getClassSize(String className) {
+        if (this.record.containsKey(className)) {
+            return this.record.get(className).size() * 4 + 4;
+        }
+        return 4;
+    }
+
+    // return corresponding class record
     public ArrayList<String> getRecord(String className) {
         return this.record.get(className);
     }
 
-    // returns corresponding class v-table
+    // return corresponding class v-table
     public Map<String, String> getVTable(String className) {
         return this.vTable.get(className);
+    }
+
+    public int getMethodOffset(String className, String method) {
+        int index = 0;
+        Map<String, String> vTable = this.vTable.get(className);
+        for (String key : vTable.keySet()) {
+            if (key == method && vTable.get(key) == className) {
+                break;
+            }
+            ++index;
+        }
+
+        return index * 4;
+    }
+
+    public int getFieldOffset(String className, String field) {
+        return this.record.get(className).lastIndexOf(field) * 4 + 4;
     }
 
     // put class name and fields list
@@ -49,7 +74,6 @@ public class ClassManager {
 
     public void printMethods(String className) {
         Map<String, String> methods = this.vTable.get(className);
-        // System.out.print(className + " methods: ");
         for (String key : methods.keySet()) {
             System.out.print("  :" + methods.get(key) + ".");
             System.out.println(key);

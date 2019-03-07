602ed1449d39c3cd554782d873a52a53cb9a0231
Annie Chiou <anniechiou4@gmail.com>
2019-03-07 14:56:52 -0800

    feat: implement addNode method in FlowGraph

--- FlowGraph.java
+++ FlowGraph.java
@@ -1,10 +1,26 @@
+import java.util.Map;
+
 import cs132.vapor.ast.*;
 
 public class FlowGraph {
     private Map<Node, List<Node>> adjNodes; // edges
 
     public FlowGraph() {
-        // TODO: Constructor
+        // TODO: graph constructor
     }
 
+    // create new node for graph
+    public void addNode(VInstr instr, Set<String> def, Set<String> use) {
+        adjNodes.putIfAbsent(new Node(instr, def, use));x
+    }
+
+    public void addEdge(Node from, Node to) {
+        if (from != null && to != null) {
+            from.addSucc(to);
+        }
+    }
+
+    public void calcLiveness() { // calculate liveness
+
+    }
 }

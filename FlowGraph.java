4e07f6e9c0d82a1894ec1cf75322bb818e30046e
Annie Chiou <anniechiou4@gmail.com>
2019-03-07 15:49:38 -0800

    fix: import statments in FlowGraph

--- FlowGraph.java
+++ FlowGraph.java
@@ -1,23 +1,31 @@
-import java.util.Map;
+import java.util.*;
 
 import cs132.vapor.ast.*;
 
 public class FlowGraph {
     private Map<Node, List<Node>> adjNodes; // edges
+    private List<Node> nodeList; // nodes
 
     public FlowGraph() {
-        // TODO: graph constructor
+        this.adjNodes = new HashMap<>();
+        this.nodeList = new ArrayList<>();
     }
 
     // create new node for graph
     public void addNode(VInstr instr, Set<String> def, Set<String> use) {
-        adjNodes.putIfAbsent(new Node(instr, def, use));x
+        // adjNodes.putIfAbsent(new Node(instr, def, use));
+        this.nodeList.add(new Node(instr, def, use));
     }
 
     public void addEdge(Node from, Node to) {
         if (from != null && to != null) {
             from.addSucc(to);
         }
+    }
+
+    public Node getNode() {
+        // TODO: return
+        return null;
     }
 
     public void calcLiveness() { // calculate liveness

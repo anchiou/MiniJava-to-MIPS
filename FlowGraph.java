d1f5ef119b474251ee163af32c76faff3ed2cef3
Annie Chiou <anniechiou4@gmail.com>
2019-03-07 16:27:32 -0800

    feat: implement calcLiveness in FlowGraph

--- FlowGraph.java
+++ FlowGraph.java
@@ -13,7 +13,7 @@ public class FlowGraph {
         this.nodeList = new ArrayList<>();
     }
 
-    // create new node for graph
+    // Create new node for graph
     public void addNode(VInstr instr, Set<String> def, Set<String> use) {
         // adjNodes.putIfAbsent(new Node(instr, def, use));
         this.nodeList.add(new Node(instr, def, use));
@@ -21,7 +21,8 @@ public class FlowGraph {
 
     // Add edge
     public void addEdge(Node from, Node to) {
-        if (from != null && to != null) {
+        if (from != null && to != null && from != to) {
+            to.addPred(from);
             from.addSucc(to);
         }
     }
@@ -32,8 +33,47 @@ public class FlowGraph {
         return null;
     }
 
-    // Calculate the liveness
-    public void calcLiveness() { // calculate liveness
+    // Calculate liveness
+    public void calcLiveness() {
+        Map<Node, Set<String>> in = new HashMap<>(); // maps nodes to live-in
+        Map<Node, Set<String>> out = new HashMap<>(); // maps nodes to live-out
 
+        for (Node node : this.nodeList) {
+            in.put(node, new HashSet<>());
+            out.put(node, new HashSet<>());
+        }
+
+        Set<String> prevIn, prevOut, newIn, newOut; // in'[n], out'[n], in[n], out[n]
+        Set<String> tempDef; // temporary def[n]
+        boolean isUpdated;
+
+        do {
+            isUpdated = false;
+            for (Node node : this.nodeList) {
+                prevIn = new HashSet<>(in.get(node));     // copy old in values
+                prevOut = new HashSet<>(out.get(node));   // copy old out values
+
+                // in[n] <-- use[n] ∪ (out[n] − def [n])
+                newIn = node.getUseSet(); // all variables in use[n]
+                tempDef = new HashSet(prevOut);
+                tempDef.removeAll(node.getDefSet()); // remove all variables in out[n] that are in def[n]
+                newIn.addAll(tempDef); // use[n] ∪ (out[n] − def [n])
+
+                // out[n] <-- U_s∈succ[n] in[s]
+                newOut = new HashSet<>();
+                for (Node succ : node.getSucc()) {
+                    newOut.addAll(in.get(succ)); // union of all in[n] for each n in succ[n]
+                }
+
+                in.put(node, newIn);
+                out.put(node, newOut);
+
+                if (!prevIn.equals(newIn) || !prevOut.equals(newOut)) {
+                    isUpdated = true;
+                }
+            }
+        } while (isUpdated);
+
+        return;
     }
 }
